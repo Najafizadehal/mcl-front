eca688a4da6b3713ee992d35a36168bd8144942d
A.latif <a.latifnajafizadeh@gmail.com>
2025-11-19 16:21:03 +0330

    serv addr

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { refresh } from './authService';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://mcl-backend.liara.run',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://mcl.liara.run',
   withCredentials: true,              // برای ارسال کوکی HttpOnly
 });
 
